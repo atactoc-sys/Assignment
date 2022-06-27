32cc52881ff88dbac00da224559b729da6dadf08
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 21:44:02 +0530

    Create Blooddonor.java

---
+++ Blooddonor.java
@@ -0,0 +1,28 @@
+// DETERMINING IF THE PERSON IS ELIGIBLE FOR DONATING BLOOD
+
+//IMPORTING UTIL PACKAGE FROM JAVA
+import java.util.*;
+
+public class Blooddonor {
+    public static void main (String args[]){
+        // CREATING NW SCANNER CLASS OBJECT FOR AGE
+        Scanner sc = new Scanner(System.in);
+        //INPUT OF AGE
+        int age = sc.nextInt();
+        //LOOP WITH LOGIC
+        if(age<18){
+            System.out.println("YOUR AGE NEED TO BE ATLEAST 18");
+        }else{
+            System.out.println("WAIT FOR YOUR WEIGHT MEASUREMENT");
+            Scanner ab = new Scanner(System.in);
+            int weight = ab.nextInt();
+
+            if(weight<48){
+                System.out.println("SORRY AT FIRST YOU NEED TO GAIN SOME WEIGHT");
+            }else{
+                System.out.println("YOU ARE ELIGIBLE");
+            }
+        }
+    }
+    
+}
