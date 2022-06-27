bb5c2a50885c0df221354d1bd827a6ea2dea9465
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 21:42:34 +0530

    Create Negorzeroorpossitivenum.java

---
+++ Negorzeroorpossitivenum.java
@@ -0,0 +1,21 @@
+// DETERMINE THE NUMBER
+//IMPORTING UTIL PACKAGE FROM JAVA
+import java.util.*;
+public class Negorzeroorpossitivenum {
+    public static void main (String args[]){
+        //SCANNER CLASS OBJECT IS CREATED
+        Scanner sc = new Scanner(System.in);
+        //INPUT FROM USER
+        int number = sc.nextInt();
+        // USING LOOP WITH LOGIC
+        if(number>0){
+            System.out.println("NUMBER IS POSSITIVE");
+        }else if(number==0){
+            System.out.println("NUMBER IS ZERO");
+        }else if(number<0){
+            System.out.println("NUMBER IS NEGATIVE");
+        }
+    }
+}
+
+
