bc05d0029d75cd03b075fece15ad6c854608cddc
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-12 20:08:31 +0530

    Update Emploiee.java

--- Emploiee.java
+++ Emploiee.java
@@ -19,3 +19,57 @@ public class Emploiee {
 
     }
 }
+
+package com.emploiee;
+
+public class Manager extends Emploiee{
+     Manager (long Id, String Name, String Address, long Phone, double basicSalary){
+          super(Id, Name, Address, Phone, basicSalary);
+
+     }
+     public void calculateTransportsAllowance(){
+          double transportAllowance = 15*basicSalary/100;
+          System.out.println("Transport allowance for Manager  :  "+transportAllowance);
+     }
+     public void calculateSalary(){
+
+     }
+
+
+}
+
+package com.emploiee;
+
+public class Trainee extends Emploiee {
+
+
+
+    Trainee(long Id, String Name, String Address, long Phone, double basicSalary) {
+        super(Id, Name, Address, Phone, basicSalary);
+    }
+    public void calculateTransportAllowance(){
+        double transportAllowance = 10*basicSalary/100;
+        System.out.println("Transport allowance for Manager  :  "+transportAllowance);
+    }
+    public void calculateSalary(){
+
+    }
+}
+
+package com.emploiee;
+
+
+public class InheritanceActivity {
+    public static void main(String args[]){
+        System.out.println("Manager :- ");
+        Manager obj1 = new Manager(126534,"Peter","Chennai India",237544,65000);
+        obj1.calculateSalary();
+        obj1.calculateTransportsAllowance();
+
+        System.out.println("Trainee :- ");
+        Trainee obj2 = new Trainee(29846,"Jack","Mumbai India",442085,45000);
+        obj2.calculateSalary();
+        obj2.calculateTransportAllowance();
+    }
+
+}
